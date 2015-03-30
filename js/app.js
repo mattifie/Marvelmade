b7c501ab277934323af08204ce6a4dc7590b613b
Marta <mattifie@gmail.com>
2015-03-30 13:05:56 -0500

    Revised Design & Hyperlink Additions

--- js/app.js
+++ js/app.js
@@ -2,31 +2,19 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
-var ctx = document.getElementById("TotalDonations").getContext("2d");
-var myNewChart = new Chart(ctx[1]).Doughnut(data,options);
-
-var data = [
-    {
-        value: 300,
-        color:"#F7464A",
-        highlight: "#FF5A5E",
-        label: "Red"
-    },
-    {
-        value: 50,
-        color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Green"
-    },
-    {
-        value: 100,
-        color: "#FDB45C",
-        highlight: "#FFC870",
-        label: "Yellow"
-    }
+// Doughnut Chart for Weekly Goals
+var TotalDonations = [
+  {
+    value: 1122,
+    color:"#a7dbd8"
+  },
+  {
+    value : 4378,
+    color : "#E2EAE9"
+  }
 ]
-
-//Get context with jQuery - using jQuery's .get() method.
-var TotalDonations = $("#TotalDonations").get(0).getContext("2d");
-//This will get the first returned node in the jQuery collection.
-var myNewChart = new Chart(TotalDonations).Doughnut(TotalDonations, chartOptions);
+var options = {
+  percentageInnerCutout : 60
+};
+var ToDate = $("#TotalDonations").get(0).getContext("2d");
+var myTotalDonations = new Chart(ToDate).Doughnut(TotalDonations, options);
